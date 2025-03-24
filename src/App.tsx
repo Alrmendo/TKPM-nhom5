cc9f4f477f95ee601b4dcf5874c309de134eb029
Audrey <dhyen22@clc.fitus.edu.vn>
2025-03-24 15:26:21 +0700

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ interface RouteConfig {
 
 // Lazy load các trang
 const Home = lazy(() => import('./pages/Home/Home'));
+const NotFoundPage = lazy(() => import('./pages/404/404'));
 // const PDP = lazy(() => import('./pages/PDP/PDP'));
 // const PCP = lazy(() => import('./pages/PCP/PCP'));
 // const Review = lazy(() => import('./pages/Payment/Review'));
@@ -29,6 +30,7 @@ const routes: RouteConfig[] = [
   // { path: '/payment-checkout', element: <Checkout /> },
   // { path: '/payment-successful', element: <Successful /> },
   { path: '/search', element: <SearchOverlay /> },
+  { path: '*', element: <NotFoundPage /> }, // Route cho trang 404
 ];
 
 const App: React.FC = () => {
